bf748c713b80fcb4472b34725ad107013e76e4ca
haennes <hannes.hofmuth@gmail.com>
2024-11-09 18:08:53 +0100

    add alias wggn

--- flake.nix
+++ flake.nix
@@ -9,28 +9,43 @@
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
 
-      packages.x86_64-linux.wgg = pkgs.writeShellApplication {
-        name = "wgg";
-        checkPhase = ":"; # too many failed checks
-        bashOptions = [ ]; # unbound variable $1
-        runtimeInputs = with pkgs; [ wireguard-tools ];
-        text = builtins.readFile ./wgg.sh;
-      };
+      packages.x86_64-linux = rec {
+        wgg = pkgs.writeShellApplication {
+          name = "wgg";
+          checkPhase = ":"; # too many failed checks
+          bashOptions = [ ]; # unbound variable $1
+          runtimeInputs = with pkgs; [ wireguard-tools ];
+          text = builtins.readFile ./wgg.sh;
+        };
+        wggn = pkgs.writeShellApplication {
 
-      packages.x86_64-linux.default = self.packages.x86_64-linux.wgg;
+          name = "wggn";
+          checkPhase = ":"; # too many failed checks
+          bashOptions = [ ]; # unbound variable $1
+          runtimeInputs = with pkgs; [ wireguard-tools ];
+          text = ''${wgg}/bin/wgg -n "$@"'';
+        };
+
+        default = self.packages.x86_64-linux.wgg;
+      };
 
       nixosModules.default = { config, lib, ... }:
         let
           inherit (lib) concatStringsSep mkEnableOption;
           cfg = config.wg-friendly-peer-names;
         in {
-          options.wg-friendly-peer-names.enable =
-            mkEnableOption "wg-friendly-peer-names";
-          config = lib.mkIf cfg.enable {
+          options.wg-friendly-peer-names = {
+            enable = mkEnableOption "wg-friendly-peer-names";
+            wggn.enable = mkEnableOption "alias for wgg -n";
+          };
+          config = lib.mkIf (cfg.enable || cfg.wggn.enable) {
             environment.etc."wireguard/peers".text = (concatStringsSep "\n"
               (map (peer: "${peer.publicKey}:${peer.name}")
                 config.networking.wireguard.interfaces.wg0.peers));
-            environment.systemPackages = [ self.packages.x86_64-linux.default ];
+            environment.systemPackages = (lib.optionals (cfg.enable)
+              [ self.packages.x86_64-linux.default ])
+              ++ (lib.optionals (cfg.wggn.enable))
+              [ self.packages.x86_64-linux.wggn ];
           };
         };
 
